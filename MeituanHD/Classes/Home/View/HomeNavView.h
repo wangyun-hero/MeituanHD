5740c9e7733eeba82dfcc9e8c355b5d7679aec49
王云 <wangyun120411@163.com>
2016-09-14 23:20:17 +0800

    自定义导航栏左边的view

---
+++ MeituanHD/Classes/Home/View/HomeNavView.h
@@ -0,0 +1,13 @@
+//
+//  HomeNavView.h
+//  MeituanHD
+//
+//  Created by 王云 on 16/9/14.
+//  Copyright © 2016年 王云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeNavView : UIView
+
+@end
---
+++ MeituanHD/Classes/Home/View/HomeNavView.m
@@ -0,0 +1,21 @@
+//
+//  HomeNavView.m
+//  MeituanHD
+//
+//  Created by 王云 on 16/9/14.
+//  Copyright © 2016年 王云. All rights reserved.
+//
+
+#import "HomeNavView.h"
+
+@implementation HomeNavView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
